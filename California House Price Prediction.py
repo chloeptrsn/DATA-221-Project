2e16301ee2b087735c775db0a8adbd039b3acaa0
NganTranAlex <tranthanhngan.2906.alex@gmail.com>
2026-03-30 12:38:39 -0600

    added import statements

---
+++ California House Price Prediction.py
@@ -0,0 +1,6 @@
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+
