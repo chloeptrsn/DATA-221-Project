7b896e1e9a4fe27376fdba9e6e3fcdbe669e2faf
cha-lin2005 <chaelynapquiz@gmail.com>
2026-03-30 12:03:36 -0600

    added and removed stuff, was gonna do standard scaler but missed the mapping part

--- Linear Regression Model - California Housing Prices - Chaelyn Apquiz.py
+++ Linear Regression Model - California Housing Prices - Chaelyn Apquiz.py
@@ -8,16 +8,21 @@ from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 
 california_house_prices = pd.read_csv("housing.csv")
 
-# find the median
-california_house_prices["total_bedrooms"] = (california_house_prices["total_bedrooms"].
-                                             fillna(california_house_prices["total_bedrooms"].median()))
+# find the median and fill in the missing values
+california_house_prices["total_bedrooms"] = (california_house_prices["total_bedrooms"].fillna(california_house_prices["total_bedrooms"].median()))
 
-feature_matrix = california_house_prices.loc[:, ['number of rooms', 'house size']]
-target_prices = california_house_prices.loc[:, ['price']]
+# converting categorical data which is the ocean_proximity column
+california_house_prices = pd.get_dummies(california_house_prices, columns=["ocean_proximity"])
 
-features_train, features_test, prices_train, prices_test = train_test_split(
-    feature_matrix,
-    target_prices,
-    test_size=0.33,
-    random_state=42
-)
+#splitting the features and target (x and y)
+feature_matrix = california_house_prices.drop("median_house_value", axis=1)
+target_prices = california_house_prices["median_house_value"]
+
+#training data, test split 70/30
+features_train, features_test, prices_train, prices_test = train_test_split(feature_matrix, target_prices, test_size=0.33, random_state=42)
+
+# scaling the values
+scaled_values = StandardScaler()
+
+
+#git push -u origin main
